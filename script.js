186876f7cf58b095d8219613e0d298c0fbf7fd87
Kristen Scheel <kristen.scheel@gmail.com>
2020-07-21 20:42:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+
+// HOW LONG TO DECOMPOSE 
+
+
+const myApp = {};
+//code here 
+
+//intro page appears, includes a paragraph about the decomposition of articles of clothing, user clicks on "get started" button to load the next page
+//baseline page includes three items, each with a toggle button (set to off)
+//event listener listens for user to toggle on or off each of the three items in the list (checkbox).
+//when an item in the list is checked (toggled on), display the data on the page in text format.
+//if the item is unchecked (toggled off), remove the corresponding text from the page.
+//if a second item is toggled on, add the corresponding data to the page in text format.
+//if a third item is toggled on, add the third item to the page in text format.
+
+//reset button - if the user clicks the reset button, return all of the toggles back to off and remove all data from the page.
+
+//Document ready: 
+$(function () {
+  myApp.init();
+ });
