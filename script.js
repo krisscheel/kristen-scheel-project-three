fcc57cf470d27d7b585e53b87df2d0529e68feb6
Kristen Scheel <kristen.scheel@gmail.com>
2020-07-23 19:59:01 -0400

    functions working

--- script.js
+++ script.js
@@ -3,14 +3,7 @@
 //intro page appears, includes a paragraph about the decomposition of articles of clothing, user clicks on "get started" button to load the next page
 //baseline page includes three items, each with a toggle button (set to off)
 //event listener listens for user to toggle on or off each of the three items in the list (checkbox).
-$('input.checkbox').change(function () {
-  if ($(this).is(':checked')) {
-    console.log('box is checked');
-  }
-  else {
-    console.log('box is NOT checked');
-  }
-});
+
 //when an item in the list is checked (toggled on), display the data on the page in text format.
 //if the item is unchecked (toggled off), remove the corresponding text from the page.
 //if a second item is toggled on, add the corresponding data to the page in text format.
@@ -20,5 +13,34 @@ $('input.checkbox').change(function () {
 
 //Document ready: 
 $(function () {
- 
+   $('#switch1').change(function () {
+    console.log('here');
+    if ($(this).is(':checked')) {
+      console.log('box is 1 checked');
+    }
+    else {
+      console.log('box is 1 NOT checked');
+    }
+  });
+
+  $('#switch2').change(function () {
+    console.log('here');
+    if ($(this).is(':checked')) {
+      console.log('box 2 is checked');
+    }
+    else {
+      console.log('box 2 is NOT checked');
+    }
+  });
+
+  $('#switch3').change(function () {
+    console.log('here');
+    if ($(this).is(':checked')) {
+      console.log('box 3 is checked');
+    }
+    else {
+      console.log('box 3 is NOT checked');
+    }
+  });
+  
  });
